d7632395acdcfd39b11ffbb24abe1a640d294579
VisalJey <mtroz310@gmail.com>
2020-03-04 23:20:23 +0700

    Initial commit

---
+++ src/app/main/sample/sample.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
+
+import { locale as english } from './i18n/en';
+import { locale as turkish } from './i18n/tr';
+import { FuseConfigService } from '@fuse/services/config.service';
+
+@Component({
+    selector   : 'sample',
+    templateUrl: './sample.component.html',
+    styleUrls  : ['./sample.component.scss']
+})
+export class SampleComponent
+{
+    /**
+     * Constructor
+     *
+     * @param {FuseTranslationLoaderService} _fuseTranslationLoaderService
+     */
+    constructor(
+        private _fuseConfigService: FuseConfigService,
+        private _fuseTranslationLoaderService: FuseTranslationLoaderService
+    )
+    {
+        this._fuseTranslationLoaderService.loadTranslations(english, turkish);
+        this._fuseConfigService.config = {
+            layout: {
+                navbar   : {
+                    hidden: true
+                },
+                toolbar  : {
+                    hidden: true
+                },
+                footer   : {
+                    hidden: true
+                },
+                sidepanel: {
+                    hidden: true
+                }
+            }
+        };
+    }
+}
